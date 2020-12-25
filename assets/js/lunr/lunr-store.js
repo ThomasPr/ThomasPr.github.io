40941fdc4f60854aab5723b3fd949b79cab670cc
ThomasPr <ThomasPr@users.noreply.github.com>
2020-12-25 16:10:48 +0000

    jekyll build from Action 7d91b8c913e1a4ffe0c760d2dee4b81145566634

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "How to setup a blog like this - Part 1: The Decision",
+        "excerpt":"I’m a person who really thinks twice about the future development of his decisions, especially when it comes to a system that I probably use for years. It took me ages to set up a blog like this, but in the end it was just a matter of minutes. In...","categories": ["blog"],
+        "tags": [],
+        "url": "https://thomas.preissler.me/blog/2020/12/20/how-to-setup-a-blog-like-this-part-1-the-decision.html",
+        "teaser": null
+      }]
