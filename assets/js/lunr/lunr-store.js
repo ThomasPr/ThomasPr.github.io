2f8a60a67ffb96d4edfef11ece786bcfea2043c2
ThomasPr <ThomasPr@users.noreply.github.com>
2020-12-26 21:04:18 +0000

    jekyll build from Action ff6e83dc7e173f3e3546653f4127a445909128d0

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,7 +1,13 @@
 var store = [{
-        "title": "How to setup a blog like this - Part 1: The Decision",
+        "title": "Make decisions about a blogging system",
         "excerpt":"I’m a person who really thinks twice about the future development of his decisions, especially when it comes to a system that I probably use for years. It took me ages to set up a blog like this, but in the end it was just a matter of minutes. In...","categories": ["blog"],
         "tags": [],
-        "url": "https://thomas.preissler.me/blog/2020/12/20/how-to-setup-a-blog-like-this-part-1-the-decision.html",
+        "url": "https://thomas.preissler.me/blog/2020/12/20/make-decisions-about-a-blogging-system.html",
+        "teaser": null
+      },{
+        "title": "Build my Jekyll blog with bare GitHub Actions",
+        "excerpt":"I use Jekyll to create my blog. It allows me to focus on writing the content with markdown and afterwards to generate a static HTML page that can be published easily. Next to Jekyll there are three additional components, all provided by GitHub: source control for the raw blog content...","categories": ["blog"],
+        "tags": [],
+        "url": "https://thomas.preissler.me/blog/2020/12/26/build-my-jekyll-blog-with-bare-github-actions.html",
         "teaser": null
       }]
