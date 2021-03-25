a7d993d208fb9edf8da4eb964c35bec2bfe43081
ThomasPr <ThomasPr@users.noreply.github.com>
2021-03-25 22:08:50 +0000

    jekyll build from Action 709e9067acabf847a72ecd1424095b8bc7b3ba4c

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -2,24 +2,30 @@ var store = [{
         "title": "Make decisions about a blogging system",
         "excerpt":"I’m a person who really thinks twice about the future development of his decisions, especially when it comes to a system that I probably use for years. It took me ages to set up a blog like this, but in the end it was just a matter of minutes. In...","categories": ["blog"],
         "tags": [],
-        "url": "https://thomas.preissler.me/blog/2020/12/20/make-decisions-about-a-blogging-system.html",
+        "url": "/blog/2020/12/20/make-decisions-about-a-blogging-system.html",
         "teaser": null
       },{
         "title": "Build my Jekyll blog with bare GitHub Actions",
         "excerpt":"I use Jekyll to create my blog. It allows me to focus on writing the content with markdown and afterwards to generate a static HTML page that can be published easily. Next to Jekyll there are three additional components, all provided by GitHub: source control for the raw blog content...","categories": ["blog"],
         "tags": [],
-        "url": "https://thomas.preissler.me/blog/2020/12/26/build-my-jekyll-blog-with-bare-github-actions.html",
+        "url": "/blog/2020/12/26/build-my-jekyll-blog-with-bare-github-actions.html",
         "teaser": null
       },{
         "title": "Build a Cartesian Product with Java Streams",
         "excerpt":"Recently, I ran over an old post by Baeldung: Permutations of an Array in Java . He presented very well solutions, as usual. I just want to provide another point of view: Readability. IMHO, readability is one of the most important aspects for code. Some time ago I had to...","categories": ["blog"],
         "tags": [],
-        "url": "https://thomas.preissler.me/blog/2020/12/29/permutations-using-java-streams.html",
+        "url": "/blog/2020/12/29/permutations-using-java-streams.html",
         "teaser": null
       },{
         "title": "Predicate.not for filtering Java Streams",
         "excerpt":"Let’s start with an example: userList.stream() .filter(user -&gt; user != null) .filter(user -&gt; !user.isActivated()) .count(); The idea of this code snippet is to count all deactivated users. At first we have to filter all nulls and secondly remove all activated users. The remaining list entries can just be counted. I...","categories": ["blog"],
         "tags": [],
-        "url": "https://thomas.preissler.me/blog/2021/01/10/predicate-not-for-filtering-java-streams.html",
+        "url": "/blog/2021/01/10/predicate-not-for-filtering-java-streams.html",
+        "teaser": null
+      },{
+        "title": "Java Streams are not always a good fit for readability",
+        "excerpt":"I really like functional programming, it offers powerful expressions with only a few lines of code. When Streams have been introduced in Java 8, they where a huge improvement for someone like me who got used to ruby and its functional power for quite some time. Even if Java Streams...","categories": ["blog"],
+        "tags": [],
+        "url": "/blog/2021/03/25/java-streams-are-not-always-a-good-fit-for-readability.html",
         "teaser": null
       }]
