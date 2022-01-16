801d7abbd80a67d63c5d91e7384f668d6232b204
ThomasPr <ThomasPr@users.noreply.github.com>
2022-01-16 20:37:06 +0000

    jekyll build from Action ce05b6e8be871427f4b76d14b763367c9f30939c

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,42 +1,42 @@
 var store = [{
         "title": "Make decisions about a blogging system",
-        "excerpt":"I’m a person who really thinks twice about the future development of his decisions, especially when it comes to a system that I probably use for years. It took me ages to set up a blog like this, but in the end it was just a matter of minutes. In...","categories": ["blog"],
+        "excerpt":"I’m a person who really thinks twice about the future development of his decisions, especially when it comes to a system that I probably use for years. It took me ages to set up a blog like this, but in the end it was just a matter of minutes. In...","categories": [],
         "tags": [],
         "url": "/blog/2020/12/20/make-decisions-about-a-blogging-system",
         "teaser": null
       },{
         "title": "Build my Jekyll blog with bare GitHub Actions",
-        "excerpt":"I use Jekyll to create my blog. It allows me to focus on writing the content with markdown and afterwards to generate a static HTML page that can be published easily. Next to Jekyll there are three additional components, all provided by GitHub: source control for the raw blog content...","categories": ["blog"],
+        "excerpt":"I use Jekyll to create my blog. It allows me to focus on writing the content with markdown and afterwards to generate a static HTML page that can be published easily. Next to Jekyll there are three additional components, all provided by GitHub: source control for the raw blog content...","categories": [],
         "tags": [],
         "url": "/blog/2020/12/26/build-my-jekyll-blog-with-bare-github-actions",
         "teaser": null
       },{
         "title": "Build a Cartesian Product with Java Streams",
-        "excerpt":"Recently, I ran over an old post by Baeldung: Permutations of an Array in Java . He presented very well solutions, as usual. I just want to provide another point of view: Readability. IMHO, readability is one of the most important aspects for code. Some time ago I had to...","categories": ["blog"],
+        "excerpt":"Recently, I ran over an old post by Baeldung: Permutations of an Array in Java . He presented very well solutions, as usual. I just want to provide another point of view: Readability. IMHO, readability is one of the most important aspects for code. Some time ago I had to...","categories": [],
         "tags": [],
         "url": "/blog/2020/12/29/permutations-using-java-streams",
         "teaser": null
       },{
         "title": "Predicate.not for filtering Java Streams",
-        "excerpt":"Let’s start with an example: userList.stream() .filter(user -&gt; user != null) .filter(user -&gt; !user.isActivated()) .count(); The idea of this code snippet is to count all deactivated users. At first we have to filter all nulls and secondly remove all activated users. The remaining list entries can just be counted. I...","categories": ["blog"],
+        "excerpt":"Let’s start with an example: userList.stream() .filter(user -&gt; user != null) .filter(user -&gt; !user.isActivated()) .count(); The idea of this code snippet is to count all deactivated users. At first we have to filter all nulls and secondly remove all activated users. The remaining list entries can just be counted. I...","categories": [],
         "tags": [],
         "url": "/blog/2021/01/10/predicate-not-for-filtering-java-streams",
         "teaser": null
       },{
         "title": "Java Streams are not always a good fit for readability",
-        "excerpt":"I really like functional programming, it offers powerful expressions with only a few lines of code. When Streams have been introduced in Java 8, they where a huge improvement for someone like me who got used to ruby and its functional power for quite some time. Even if Java Streams...","categories": ["blog"],
+        "excerpt":"I really like functional programming, it offers powerful expressions with only a few lines of code. When Streams have been introduced in Java 8, they where a huge improvement for someone like me who got used to ruby and its functional power for quite some time. Even if Java Streams...","categories": [],
         "tags": [],
         "url": "/blog/2021/03/25/java-streams-are-not-always-a-good-fit-for-readability",
         "teaser": null
       },{
         "title": "Release memory back to the OS with Java 11",
-        "excerpt":"I’m responsible for a Java application running in OpenShift. This application has to process a huge amount of data occassionally, but most of the time the application idles and waits for new input. The application takes a huge amount of memory during processing the data. But when the processing job...","categories": ["blog"],
+        "excerpt":"I’m responsible for a Java application running in OpenShift. This application has to process a huge amount of data occassionally, but most of the time the application idles and waits for new input. The application takes a huge amount of memory during processing the data. But when the processing job...","categories": [],
         "tags": [],
         "url": "/blog/2021/05/02/release-memory-back-to-the-os-with-java-11",
         "teaser": null
       },{
         "title": "Implementing a timeline with scrolling and zooming - or how I failed at elementary school math",
-        "excerpt":"Recently I had the task of implementing a timeline. Not a big deal? You’re probably right, it’s just a bit of Flexbox magic. But there are three challenges: The events in the timeline are not equally distributed and the space between the events need to follow that. Since the timeline...","categories": ["blog"],
+        "excerpt":"Recently I had the task of implementing a timeline. Not a big deal? You’re probably right, it’s just a bit of Flexbox magic. But there are three challenges: The events in the timeline are not equally distributed and the space between the events need to follow that. Since the timeline...","categories": [],
         "tags": [],
         "url": "/blog/2022/01/10/implementing-a-timeline-with-scrolling-and-zooming-or-how-i-failed-at-elementary-school-math",
         "teaser": null
